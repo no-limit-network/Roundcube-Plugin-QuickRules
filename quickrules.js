003d12cacc4a7d601e16c9b5d23b94f7853fd75b
PhilW <roundcube@tehinterweb.co.uk>
2011-01-14 19:23:39 +0000

    first commit

---
+++ quickrules.js
@@ -0,0 +1,136 @@
+/**
+ * QuickRules plugin script
+ */
+
+if (window.rcmail) {
+	rcmail.addEventListener('init', function(evt) {
+		// register command (directly enable in message view mode)
+		rcmail.register_command('plugin.quickrules.create', rcmail_quickrules, rcmail.env.uid);
+
+		if (rcmail.message_list && rcmail.env.junk_mailbox) {
+			rcmail.message_list.addEventListener('select', function(list) {
+				rcmail.enable_command('plugin.quickrules.create', list.get_selection().length > 0);
+			});
+		}
+	})
+}
+
+function rcmail_quickrules() {
+	if (!rcmail.env.uid && (!rcmail.message_list || !rcmail.message_list.get_selection().length))
+		return;
+
+	var prev_sel = null;
+
+	// also select childs of (collapsed) threads
+	if (rcmail.message_list) {
+		if (rcmail.env.uid) {
+			if (rcmail.message_list.rows[rcmail.env.uid].has_children && !rcmail.message_list.rows[rcmail.env.uid].expanded) {
+				if (!rcmail.message_list.in_selection(rcmail.env.uid)) {
+					prev_sel = rcmail.message_list.get_selection();
+					rcmail.message_list.select_row(rcmail.env.uid);
+				}
+
+				rcmail.message_list.select_childs(rcmail.env.uid);
+				rcmail.env.uid = null;
+			}
+			else if (rcmail.message_list.get_single_selection() == rcmail.env.uid) {
+				rcmail.env.uid = null;
+			}
+		}
+		else {
+			selection = rcmail.message_list.get_selection();
+			for (var i in selection) {
+				if (rcmail.message_list.rows[selection[i]].has_children && !rcmail.message_list.rows[selection[i]].expanded)
+					rcmail.message_list.select_childs(selection[i]);
+			}
+		}
+	}
+
+	var uids = rcmail.env.uid ? rcmail.env.uid : rcmail.message_list.get_selection().join(',');
+
+	var lock = rcmail.set_busy(true, 'loading');
+	rcmail.http_post('plugin.quickrules.add', '_uid='+uids+'&_mbox='+urlencode(rcmail.env.mailbox), lock);
+
+	if (prev_sel) {
+		rcmail.message_list.clear_selection();
+
+		for (var i in prev_sel)
+			rcmail.message_list.select_row(prev_sel[i], CONTROL_KEY);
+	}
+}
+
+function quickrules_add_filter() {
+	rcmail.command('plugin.sieverules.add');
+}
+
+function quickrules_setup_rules() {
+	var rulesTable = rcube_find_object('rules-table');
+	var actsTable = rcube_find_object('actions-table');
+
+	if (rcmail_quickrules_rules.length < 1)
+		return;
+
+	for (i = 1; i < rcmail_quickrules_rules.length; i++)
+		rcmail.command('plugin.sieverules.add_rule','', rulesTable.tBodies[0].rows[0]);
+
+	var headers = document.getElementsByName('_selheader[]');
+	var ops = document.getElementsByName('_operator[]');
+	var targets = document.getElementsByName('_target[]');
+
+	for (var i = 1; i < headers.length; i++) {
+		$(headers[i]).val(rcmail_quickrules_rules[i-1].header);
+		$(ops[i]).val(rcmail_quickrules_rules[i-1].op);
+
+		// check values set ok before adding rule
+		if ($(headers[i]).val() == rcmail_quickrules_rules[i-1].header && $(ops[i]).val() == rcmail_quickrules_rules[i-1].op) {
+			rcmail.sieverules_header_select(headers[i]);
+
+			// set the op again (header onchange resets it)
+			$(ops[i]).val(rcmail_quickrules_rules[i-1].op);
+			rcmail.sieverules_rule_op_select(ops[i]);
+
+			targets[i].value = rcmail_quickrules_rules[i-1].target;
+		}
+		else {
+			headers[i].selectedIndex = 0;
+			ops[i].selectedIndex = 0;
+		}
+	}
+
+	if (rcmail_quickrules_actions.length < 1)
+		return;
+
+	for (i = 1; i < rcmail_quickrules_actions.length; i++)
+		rcmail.command('plugin.sieverules.add_action','', actsTable.tBodies[0].rows[0]);
+
+	var acts = document.getElementsByName('_act[]');
+	var folders = document.getElementsByName('_folder[]');
+
+	for (var i = 1; i < acts.length; i++) {
+		$(acts[i]).val(rcmail_quickrules_actions[i-1].act);
+
+		// check for imap4flags
+		if (rcmail_quickrules_actions[i-1].act == 'imapflags' && $(acts[i]).val() != rcmail_quickrules_actions[i-1].act)
+			$(acts[i]).val('imap4flags');
+
+		// check values set ok before adding action
+		if ($(acts[i]).val() == rcmail_quickrules_actions[i-1].act) {
+			rcmail.sieverules_action_select(acts[i]);
+			$(folders[i]).val(rcmail_quickrules_actions[i-1].props);
+		}
+		else {
+			acts[i].selectedIndex = 0;
+		}
+	}
+}
+
+$(document).ready(function() {
+	if (rcmail.env.action == 'plugin.sieverules')
+		rcmail.add_onload('quickrules_add_filter();');
+
+	if (rcmail.env.action == 'plugin.sieverules.add')
+		rcmail.add_onload('quickrules_setup_rules();');
+
+	if (window.rcm_contextmenu_register_command)
+		rcm_contextmenu_register_command('quickrules', 'rcmail_quickrules', rcmail.gettext('quickrules.createfilter'), 'moveto', 'after', true);
+});
---
+++ quickrules.php
@@ -0,0 +1,112 @@
+<?php
+
+/**
+ * QuickRules
+ *
+ * Plugin to allow the user to quickly create filters from the message list
+ *
+ * @version 0.1
+ * @author Philip Weir
+ * @requires SieveRules plugin
+ */
+class quickrules extends rcube_plugin
+{
+	public $task = 'mail|settings';
+
+	// default values: label => value
+	private $headers = array('subject' => 'header::Subject',
+					'from' => 'address::From',
+					'to' => 'address::To',
+					'cc' => 'address::Cc',
+					'bcc' => 'address::Bcc',
+					'envelopeto' => 'envelope::To',
+					'envelopefrom' => 'envelope::From'
+					);
+
+	private $operators = array('filtercontains' => 'contains',
+					'filternotcontains' => 'notcontains',
+					'filteris' => 'is',
+					'filterisnot' => 'notis',
+					'filterexists' => 'exists',
+					'filternotexists' => 'notexists'
+					);
+
+	function init()
+	{
+		$rcmail = rcmail::get_instance();
+		$this->register_action('plugin.quickrules.add', array($this, 'init_rule'));
+
+		if ($rcmail->task == 'mail' && ($rcmail->action == '' || $rcmail->action == 'show')) {
+			$this->add_texts('localization', true);
+			$this->include_script('quickrules.js');
+			$this->include_stylesheet($this->local_skin_path() .'/quickrules.css');
+			if ($rcmail->output->browser->ie && $rcmail->output->browser->ver == 6)
+				$this->include_stylesheet($this->local_skin_path() . '/ie6hacks.css');
+
+			$this->add_button(array('command' => 'plugin.quickrules.create', 'type' => 'link', 'class' => 'buttonPas quickrules', 'classact' => 'button quickrules', 'classsel' => 'button quickrulesSel', 'title' => 'quickrules.createfilter', 'content' => ' '), 'toolbar');
+		}
+
+		if ($_SESSION['plugin.quickrules'])
+			$this->_create_rule();
+	}
+
+	function init_rule()
+	{
+		$_SESSION['plugin.quickrules'] = true;
+		$_SESSION['plugin.quickrules.uids'] = get_input_value('_uid', RCUBE_INPUT_POST);
+		$_SESSION['plugin.quickrules.mbox'] = get_input_value('_mbox', RCUBE_INPUT_POST);
+
+		rcmail::get_instance()->output->redirect(array('task' => 'settings', 'action' => 'plugin.sieverules'));
+	}
+
+	private function _create_rule()
+	{
+		$rcmail = rcmail::get_instance();
+		if ($rcmail->action == 'plugin.sieverules' || $rcmail->action == 'plugin.sieverules.add') {
+			$this->include_script('quickrules.js');
+
+			if ($rcmail->action == 'plugin.sieverules.add') {
+				$uids = $_SESSION['plugin.quickrules.uids'];
+				$mbox = $_SESSION['plugin.quickrules.mbox'];
+				$rcmail->imap_connect();
+
+				$rules = array();
+				$actions = array();
+				foreach (explode(",", $uids) as $uid) {
+					$message = new rcube_message($uid);
+					$rules[] = json_serialize(array('header' => $this->headers['from'], 'op' => $this->operators['filteris'], 'target' => $message->sender['mailto']));
+
+					$recipients = array();
+					$recipients_array = $rcmail->imap->decode_address_list($message->headers->to);
+					foreach ($recipients_array as $recipient)
+						$recipients[] = $recipient['mailto'];
+
+					$identity = $rcmail->user->get_identity();
+					$recipient_str = join(', ', $recipients);
+					if ($recipient_str != $identity ['email'])
+						$rules[] = json_serialize(array('header' => $this->headers['to'], 'op' => $this->operators['filteris'], 'target' => $recipient_str));
+
+					if (strlen($message->subject) > 0)
+						$rules[] = json_serialize(array('header' => $this->headers['subject'], 'op' => $this->operators['filtercontains'], 'target' => $message->subject));
+
+					if ($mbox != 'INBOX')
+						$actions[] = json_serialize(array('act' => 'fileinto', 'props' => $mbox));
+
+					foreach ($message->headers->flags as $flag) {
+						if ($flag != '')
+							$actions[] = json_serialize(array('act' => 'imapflags', 'props' => $flag));
+					}
+				}
+
+				$this->api->output->add_script(JS_OBJECT_NAME . "_quickrules_rules = [" . implode(',', $rules) . "];");
+				$this->api->output->add_script(JS_OBJECT_NAME . "_quickrules_actions = [" . implode(',', $actions) . "];");
+
+				$_SESSION['plugin.quickrules'] = false;
+				$_SESSION['plugin.quickrules.uids'] = '';
+				$_SESSION['plugin.quickrules.mbox'] = '';
+			}
+		}
+	}
+}
+
+?>
