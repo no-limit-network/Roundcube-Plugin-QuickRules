0bc56a6a5b7c8342a4da0629c98828ccb88b35c2
PhilW <roundcube@tehinterweb.co.uk>
2011-01-15 08:17:55 +0000

    add button disable code

--- quickrules.js
+++ quickrules.js
@@ -90,7 +90,35 @@ function quickrules_setup_rules() {
 	}
 }
 
-$(document).ready(function() {
+function rcmail_quickrules_status(command) {
+	switch (command) {
+		case 'beforedelete':
+			if (!rcmail.env.flag_for_deletion && rcmail.env.trash_mailbox &&
+				rcmail.env.mailbox != rcmail.env.trash_mailbox &&
+				(rcmail.message_list && !rcmail.message_list.shiftkey))
+				rcmail.enable_command('plugin.quickrules.create', false);
+
+			break;
+		case 'beforemove':
+		case 'beforemoveto':
+			rcmail.enable_command('plugin.quickrules.create', false);
+			break;
+		case 'aftermove':
+		case 'aftermoveto':
+			if (rcmail.env.action == 'show')
+				rcmail.enable_command('plugin.quickrules.create', true);
+
+			break;
+		case 'afterpurge':
+		case 'afterexpunge':
+			if (!rcmail.env.messagecount && rcmail.task == 'mail')
+				rcmail.enable_command('plugin.quickrules.create', false);
+
+			break;
+	}
+}
+
+function rcmail_quickrules_init() {
 	if (rcmail.env.action == 'plugin.sieverules')
 		rcmail.add_onload('quickrules_add_filter();');
 
@@ -99,4 +127,15 @@ $(document).ready(function() {
 
 	if (window.rcm_contextmenu_register_command)
 		rcm_contextmenu_register_command('quickrules', 'rcmail_quickrules', rcmail.gettext('quickrules.createfilter'), 'moveto', 'after', false);
-});
+}
+
+rcmail.add_onload('rcmail_quickrules_init()');
+
+// update button activation after external events
+rcmail.addEventListener('beforedelete', function(props) { rcmail_quickrules_status('beforedelete'); } );
+rcmail.addEventListener('beforemove', function(props) { rcmail_quickrules_status('beforemove'); } );
+rcmail.addEventListener('beforemoveto', function(props) { rcmail_quickrules_status('beforemoveto'); } );
+rcmail.addEventListener('aftermove', function(props) { rcmail_quickrules_status('aftermove'); } );
+rcmail.addEventListener('aftermoveto', function(props) { rcmail_quickrules_status('aftermoveto'); } );
+rcmail.addEventListener('afterpurge', function(props) { rcmail_quickrules_status('afterpurge'); } );
+rcmail.addEventListener('afterexpunge', function(props) { rcmail_quickrules_status('afterexpunge'); } );
